fef7db94e376dd935ffee4e858d55b6753152bd1
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-08-18 18:39:09 -0300

    fix production route type error

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,5 +1,6 @@
 import "server-only";
 import { db } from "./db";
+import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 
 export async function getMyImages() {
@@ -8,7 +9,7 @@ export async function getMyImages() {
   if (!user.userId) throw new Error("Unauthorized");
 
   const images = await db.query.images.findMany({
-    where: (model, { eq }) => eq(model.userId, user.userId),
+    where: (model/* , { eq } */) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.id),
   });
   return images;
@@ -19,12 +20,12 @@ export async function getImages(id: number) {
   if (!user.userId) throw new Error("Unauthorized");
 
   const image = await db.query.images.findFirst({
-    where: (model, { eq }) => eq(model.id, id),
+    where: (model/* , { eq } */) => eq(model.id, id),
   });
 
   if (!image) throw new Error("Image not found");
 
   if (image.userId !== user.userId) throw new Error("Unauthorized");
-  
+
   return image;
 }
