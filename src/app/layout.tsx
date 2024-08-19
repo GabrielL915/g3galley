6c4c1c3e15e0805e6c9d03fa27dfebc2e02bb8e8
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-08-19 08:44:17 -0300

    move to grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-        <body className="flex flex-col gap-4">
-          <TopNav />
-          {children}
+        <body className="">
+          <div className="grid h-screen grid-rows-[auto,ifr]">
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
+          </div>
           {modal}
           <div id="modal-root"></div>
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,11 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images, ...images].map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
-          <Link href={`/img/${image.id}`}>
+          <Link href={`
+          /img/${image.id}`}>
             <Image
               src={image.url}
               style={{ objectFit: "contain" }}
